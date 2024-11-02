7c6fcbf6727ae800528f4d09bf4d96646479e27f
Mai1203 <83738249+Mai1203@users.noreply.github.com>
2024-11-01 19:12:17 -0500

    init

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,18 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integration: [
+    fulldev({
+      colors: {
+        theme: 'dark',
+        dark: {
+          background: '#1f1f1f',
+          base: '#6f6d66',
+          brand: '#f50'
+        }
+      }
+    })
+  ]
+});
